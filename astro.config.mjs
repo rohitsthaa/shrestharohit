bc67005b0628d719541cd0d9e9c021cc354a755b
Rohit Shrestha <rohit@myleanlaw.com>
2025-08-24 15:54:55 +0545

    fix prod redirect issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://shrestharohit.com.np',
   // Only use base path in production (GitHub Pages)
-  base: process.env.NODE_ENV === 'production' ? '/astro-theme-terminal' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
